c8425b13dfca8bb148d520d54c5a70b39e62ae38
Ashay Sheth <ashay.sheth@gmail.com>
2020-04-30 02:41:51 -0400

    Updates

---
+++ precache-manifest.33ecd868fe7a0da943423ecdec1bb441.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e418fa15fed36f2137275d43dac2ae2c",
+    "url": "/snake-eyes/index.html"
+  },
+  {
+    "revision": "62b7000a88725b842d58",
+    "url": "/snake-eyes/static/css/main.d03bebb8.chunk.css"
+  },
+  {
+    "revision": "8834ca8d87930bc4f908",
+    "url": "/snake-eyes/static/js/2.1dabe452.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/snake-eyes/static/js/2.1dabe452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62b7000a88725b842d58",
+    "url": "/snake-eyes/static/js/main.07bf1883.chunk.js"
+  },
+  {
+    "revision": "076ade716f013512a42f",
+    "url": "/snake-eyes/static/js/runtime-main.e219c39d.js"
+  }
+]);
