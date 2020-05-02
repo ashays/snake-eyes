45c7135857ed25696ae1ca5ee198a0ef0f3b1ec9
Ashay Sheth <ashay.sheth@gmail.com>
2020-05-02 00:29:59 -0400

    added timer sound

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './Timer.css';
 
+import blop from '../assets/sounds/blop.mp3';
+
 class Timer extends Component {
     constructor(props) {
         super(props);
         this.state = {
             remaining: this.getRemainingTime(props.start)
         }
+        this.timer = this.timer.bind(this);
+        this.SCARY = 20;
     }
 
     getRemainingTime() {
@@ -14,30 +18,44 @@ class Timer extends Component {
     }
 
     componentDidMount() {
+        this.blop = new Audio(blop);
+        this.blop.loop = false;
         if (this.props.start) {
-            this.interval = setInterval(() => {
-                this.setState({remaining: this.getRemainingTime(this.props.start)});
-            }, 1000);    
+            this.timerInterval = setInterval(this.timer, 1000);
         }
     }
 
     componentDidUpdate(prevProps) {
         if (this.props.start !== prevProps.start) {
-            this.setState({remaining: this.getRemainingTime(this.props.start)});
-            this.interval = setInterval(() => {
-                this.setState({remaining: this.getRemainingTime(this.props.start)});
-            }, 1000);
+            this.timerInterval = setInterval(this.timer, 1000);
+        }
+    }
+
+    timer() {
+        let remaining = this.getRemainingTime(this.props.start);
+        this.setState({remaining});
+        if (remaining > 40 && remaining % 2 === 0) {
+            this.blop.play();
+        } else if (remaining <= 40 && remaining > this.SCARY) {
+            this.blop.play();
+        } else if (remaining <= this.SCARY) {
+            this.blop.play();
+            setTimeout(() => { this.blop.play(); }, 500);
+        } else if (remaining < 0) {
+            clearInterval(this.timerInterval);
         }
     }
 
     componentWillUnmount() {
-        clearInterval(this.interval);
+        clearInterval(this.timerInterval);
     }
     
     render() {
         if (this.state.remaining) {
             return (
-                <div className={this.state.remaining <= 10 ? "timer scary" : "timer"}>{this.state.remaining}</div>
+                <div>
+                    <div className={this.state.remaining <= this.SCARY ? "timer scary" : "timer"}>{this.state.remaining}</div>
+                </div>
             );    
         }
         return (<div></div>);
